b091cea59b05398194679c52d07be555e79ddb94
liuniuliuniu <416997919@qq.com>
2017-06-01 13:48:54 +0800

    init

---
+++ LLImagePickerDemo/LLImagePickerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LLImagePickerDemo
+//
+//  Created by liushaohua on 2017/6/1.
+//  Copyright © 2017年 liushaohua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LLImagePickerDemo/LLImagePickerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LLImagePickerDemo
+//
+//  Created by liushaohua on 2017/6/1.
+//  Copyright © 2017年 liushaohua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
